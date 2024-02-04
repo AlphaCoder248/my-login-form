3362bdbcbbe510b55bf67341288786158f4001d4
Alphacoder248 <societyofcoinape@gmail.com>
2024-02-04 23:59:42 +0530

    test

--- register.php
+++ register.php
@@ -1,14 +1,11 @@
-<!-- register.php -->
 <?php
 $servername = "localhost";
 $username = "newuser";
 $dbpassword = "admin123";
 $dbname = "login-form";
 
-// Create connection
 $conn = new mysqli($servername, $username, $dbpassword, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -18,14 +15,11 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 $confirmPassword = $_POST['confirmPassword'];
 
-// Check if passwords match
 if ($password != $confirmPassword) {
     echo "Passwords do not match";
     exit;
 }
 
-// Hash the password
-//$password = password_hash($password, PASSWORD_DEFAULT);
 
 $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
 
